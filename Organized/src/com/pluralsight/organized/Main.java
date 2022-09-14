9983feccec693d262d13b2129c02901c869f46ea
Damelio <sophie.damelio@accenture.com>
2022-09-14 14:18:15 -0700

    create calcengine app

--- Organized/src/com/pluralsight/organized/Main.java
+++ Organized/src/com/pluralsight/organized/Main.java
@@ -3,6 +3,25 @@ package com.pluralsight.organized;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("We got organized!");
+//	System.out.println("We got organized!");
+
+//        int valA = 21;
+//        int valB = 6;
+//        int valC = 3;
+//        int valD = 1;
+//
+//        int result1 = valA - valB / valC; // division first,
+//
+//        int result2 = (valA - valB) / valC;
+//
+//        System.out.println(result1);
+//        System.out.println(result2);
+
+
+
+
+
+
+
     }
 }

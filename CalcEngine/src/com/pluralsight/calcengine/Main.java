546e47f5ddea857645d3fc4b974638ad03793fe4
Damelio <sophie.damelio@accenture.com>
2022-09-15 14:55:32 -0700

    Completed module 7, updated app to allow for interactivity, and use of strings in addition to ints

--- CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -1,39 +1,130 @@
 package com.pluralsight.calcengine;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
         double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
         char[] opCodes = {'d', 'a', 's', 'm'};
         double[] results = new double[opCodes.length];
 
-        for (int i = 0; i < opCodes.length; i++) {
-            switch (opCode[i]) {
-                case 'a':
-                    results[i] = leftVals[i] + rightVals[i];
-                    break;
-                case 's':
-                    results[i] = value1 - value2;
-                    break;
-                case 'm':
-                    results[i] = value1 * value2;
-                    break;
-                case 'd':
-                    results[i] = value1 != 0 ? value1 / value2 : 0.0d;
-                    break;
-                default:
-                    System.out.println("Invalid opCode: " + opCode);
-                    results[i] = 0.0d; //  invalid op code
-                    break;
+
+        if (args.length == 0) {
+            for (int i = 0; i < opCodes.length; i++) {
+                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
+            }
+            for (double currentResult : results)
+                System.out.println(currentResult);
+        } else if (args.length == 1 && args[0].equals("interactive"))
+            executeInteractivity();
+        else if (args.length == 3)
+            handleCommandLine(args);
+        else
+            System.out.println("Please provide an operation code and 2 numeric values");
+    }
+
+    static void executeInteractivity(){
+        System.out.print("Enter an operation and two numbers:");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split( " "); // return a string array of each word as an element
+        perfromOperation(parts);
+    }
+
+    private static void perfromOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+        double result = execute(opCode, leftVal, rightVal);
+
+        displayResult(opCode, leftVal, rightVal, result);
+    }
+
+    private static void displayResult(char opCode, double leftVal, double rightVal, double result) {
+        char symbol = symbolFromOpCode(opCode);
+        StringBuilder builder = new StringBuilder(20);
+        builder.append(leftVal);
+        builder.append(" ");
+        builder.append(symbol); // char
+        builder.append(" ");
+        builder.append(rightVal);
+        builder.append(" = ");
+        builder.append(result); // double types
+
+        String output = builder.toString();
+        System.out.println(output);
+    }
+
+    private static char symbolFromOpCode(char opCode) {
+        char[] opCodes = {'a', 's', 'm', 'd'};
+        char[] symbols = {'+', '-', '*', '/'};
+        char symbol = ' ';
+        for(int index = 0; index < opCodes.length; index++){
+            if(opCode == opCodes[index]) {
+                symbol = symbols[index];
+                break;
+            }
+        }
+        return symbol;
+    }
+
+    private static void handleCommandLine(String[] args) {
+
+        char opCode = args[0].charAt(0);
+        double leftVal = Double.parseDouble(args[1]);
+        double rightVal = Double.parseDouble(args[2]); // convert string data values to correct data types
+
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
+
+    }
+
+    static double execute(char opCode, double leftVal, double rightVal) {
+        double result;
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            case 'd':
+                result = leftVal != 0 ? leftVal / rightVal : 0.0d;
+                break;
+            default:
+                System.out.println("Invalid opCode: " + opCode);
+                result = 0.0d; //  invalid op code
+                break;
+        }
+        return result;
+    }
+
+    // translate words like multiple into opcode
+    static char opCodeFromString(String operationName) {
+        char opCode = operationName.charAt(0);
+        return opCode;
+    }
+
+    static double valueFromWord(String word) {
+        String[] numberWords = {
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"
+        };
+
+        double value = 0d;
+        for (int index = 0; index < numberWords.length; index++){
+            if(word.equals(numberWords[index])) {
+                value = index;
+                break;
             }
         }
 
-
-        System.out.println(result);
-
-
-
+        return value; // this gives numberic value that corresponds to the array
     }
 }
